90eb60a00f06b84efeca5dc54e83e7957b033e31
cesar munguia <c.munguiabrito@gmail.com>
2026-04-11 18:57:42 -0600

    feat: automation

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -2,11 +2,22 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { DeployWebAppStack } from '../lib/deploy-web-app-stack';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 
 const app = new cdk.App();
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+
+const account = process.env.AWS_ACCOUNT;
+const region = process.env.AWS_REGION;
+
+if (!account || !region) {
+  throw new Error("AWS_ACCOUNT_ID or AWS_REGION not found in environment variables");
+}
+
 new DeployWebAppStack(app, 'DeployWebAppStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account, region },
 });
