e368533ae313f7915216e604c1b5d631cc33e28b
cesar munguia <c.munguiabrito@gmail.com>
2026-04-11 19:06:10 -0600

    feat: added tiny change

--- src/components/pages/PageProducts/PageProducts.tsx
+++ src/components/pages/PageProducts/PageProducts.tsx
@@ -1,9 +1,11 @@
 import Products from "~/components/pages/PageProducts/components/Products";
 import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
 
 export default function PageProducts() {
   return (
     <Box py={3}>
+      <Typography variant="h1">Products that cesar owns visit https://blackstonehobbies.com/</Typography>
       <Products />
     </Box>
   );
