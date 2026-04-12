19337657ccaab35c8457498b2f271e1a5a239eed
cesar munguia <c.munguiabrito@gmail.com>
2026-04-11 19:10:08 -0600

    feat: support warhammer

--- src/components/pages/PageProducts/PageProducts.tsx
+++ src/components/pages/PageProducts/PageProducts.tsx
@@ -5,7 +5,7 @@ import { Typography } from "@mui/material";
 export default function PageProducts() {
   return (
     <Box py={3}>
-      <Typography variant="h1">Products that cesar owns visit https://blackstonehobbies.com/</Typography>
+      <Typography variant="h4">Products that cesar owns visit <a href="https://blackstonehobbies.com/">Support Warhammer!</a></Typography>
       <Products />
     </Box>
   );
