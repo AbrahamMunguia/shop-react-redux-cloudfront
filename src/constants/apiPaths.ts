7acb1b0c94c9f750d8c15acbbf8d7832dda5c82b
cesar munguia <c.munguiabrito@gmail.com>
2026-05-02 13:29:25 -0600

    feat: task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const BASE_URL = `https://9yd6w4clbj.execute-api.us-east-1.amazonaws.com/prod`
 const API_PATHS = {
   product: `${BASE_URL}/products`,
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: `${BASE_URL}/import`,
   addProduct: `${BASE_URL}/products`,
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: `${BASE_URL}`,
