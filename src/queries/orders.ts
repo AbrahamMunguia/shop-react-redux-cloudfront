c7e089558698c9148d6a20f47d2d37535ef112b1
cesar munguia <c.munguiabrito@gmail.com>
2026-05-28 13:38:04 -0600

    refactor: update order API endpoint and switch authorization token key to basic-auth

--- src/queries/orders.ts
+++ src/queries/orders.ts
@@ -26,7 +26,7 @@ export function useUpdateOrderStatus() {
       const { id, ...data } = values;
       return axios.put(`${API_PATHS.order}/order/${id}/status`, data, {
         headers: {
-          Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
+          Authorization: `Basic ${localStorage.getItem("basic-auth")}`,
         },
       });
     }
@@ -35,9 +35,9 @@ export function useUpdateOrderStatus() {
 
 export function useSubmitOrder() {
   return useMutation((values: Omit<Order, "id">) => {
-    return axios.put<Omit<Order, "id">>(`${API_PATHS.order}/order`, values, {
+    return axios.post(`${API_PATHS.order}`, values, {
       headers: {
-        Authorization: `Basic ${localStorage.getItem("authorization_token")}`,
+        Authorization: `Basic ${localStorage.getItem("basic-auth")}`,
       },
     });
   });
